ac2862d15028cf36f347ef7109b224cd74067e5d
Eric Zhang <canyouyou99@gmail.com>
2024-03-13 11:23:43 -0700

    random work complete

--- Python Works/work_random.py
+++ Python Works/work_random.py
@@ -3,19 +3,20 @@
 # Date: Mar 11, 2024
 
 # Explain the situation
-print("Eric is prepping 10 academic tests, and he has practiced this set of tests by 1,000,000 times. He may have scored randomly from 90% to 100% for each set. If he scored half of the tests below 95%, he will get punished. If he scored half of the tests above 95%, he will get a \"keep trying\". If he got them all 100%, he will get a \"good job\" compliment. What are the odds?")
+print("Eric is prepping 10 academic tests, and he has practiced this set of tests by 1,000,000 times. He may have scored randomly from 90% to 100% for each set. If he scored half of the tests below 95%, he will get punished. If he scored half of the tests above 95%, he will get a \"keep trying\". If he got them above 95, he will get a \"good job\" compliment. What are the odds?")
 
 import random
 
 def test_odds():
 
-    ninety_five_number = random.randrange(0,10)
+    ninety_five_number = random.randrange(0,11)
+
     if ninety_five_number < 5:
         return "punish"
     elif ninety_five_number == 10:
-        return "keep_trying"
-    else:
         return "good_job"
+    else:
+        return "keep_trying"
 
 # Repeat the experiment for 1,000,000 times, and keep track of the numbers.
 
@@ -28,7 +29,7 @@ def main():
         result = test_odds()
         if result == "punish":
             punish += 1
-        elif result == "keep trying":
+        elif result == "keep_trying":
             keep_trying += 1
         else:
             good_job += 1
