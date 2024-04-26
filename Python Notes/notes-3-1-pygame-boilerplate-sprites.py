fde78ed85229d4534c0e3d681adfd1e2116eb670
Eric Zhang <canyouyou99@gmail.com>
2024-04-26 11:33:46 -0700

    Made Dvd logo move

--- Python Notes/notes-3-1-pygame-boilerplate-sprites.py
+++ Python Notes/notes-3-1-pygame-boilerplate-sprites.py
@@ -3,6 +3,19 @@
 #   - Sprite class
 
 import pygame
+import random
+
+WHITE   = (255, 255, 255)
+BLACK   = (  0,   0,   0)
+EMERALD = ( 21, 219, 147)
+RED     = (255,   0,   0)
+GREEN   = (  0, 255,   0)
+BLUE    = (  0,   0, 255)
+GRAY    = (128, 128, 128)
+
+WIDTH   = 1280    # Pixels
+HEIGHT  =  720
+SCREEN_SIZE = (WIDTH, HEIGHT)
 
 class Dvdlogo(pygame.sprite.Sprite):
     """Represents the DVD Logo"""
@@ -15,6 +28,42 @@ class Dvdlogo(pygame.sprite.Sprite):
         #   - first position of the inage is at the top left
         self.rect = self.image.get_rect()   
 
+        self.rect.centerx = random.randrange(0, 1280)
+        self.rect.centery = random.randrange(0,720)
+
+        # How much position changes over time
+        #   - pixels per tick
+        self.vel_x = 3
+        self.vel_y = 3
+
+    def update(self):
+        # Updae the position of Dvdlogo
+        self.rect.x = self.rect.x + self.vel_x
+        self.rect.y += self.vel_y
+
+        # Keep the Dvdlogo in the screen
+
+        # Right side of the screen
+        #   - if the right edge of dvdlogo > WIDTH
+        #       - swich the direction (+vel-x -> -vel-x)
+        if self.rect.right >= WIDTH:
+            self.vel_x = -self.vel_x    # or: self.vel_x *= -1
+
+        # Left side of the screen
+        if self.rect.left <= 0:
+            self.vel_x *= -1
+        
+        # Bottom of the screen
+        if self.rect.bottom >= HEIGHT:
+            self.vel_y *= -1
+        
+        # Top of the screen
+        if self.rect.top <= 0:
+            self.vel_y *= -1
+
+
+        print(self.rect.x, self.rect.y)
+
 def start():
     """Environment Setup and Game Loop"""
 
@@ -22,17 +71,7 @@ def start():
 
     # --CONSTANTS--
     # COLOURS
-    WHITE   = (255, 255, 255)
-    BLACK   = (  0,   0,   0)
-    EMERALD = ( 21, 219, 147)
-    RED     = (255,   0,   0)
-    GREEN   = (  0, 255,   0)
-    BLUE    = (  0,   0, 255)
-    GRAY    = (128, 128, 128)
 
-    WIDTH   = 1280    # Pixels
-    HEIGHT  =  720
-    SCREEN_SIZE = (WIDTH, HEIGHT)
 
     # --VARIABLES--
     screen = pygame.display.set_mode(SCREEN_SIZE)
@@ -42,8 +81,8 @@ def start():
     dvdlogo = Dvdlogo()
 
     # Move the DVD logo to the middle-ish
-    dvdlogo.rect.centerx = WIDTH // 2   # // always gives an integer
-    dvdlogo.rect.centery = HEIGHT // 2
+    # dvdlogo.rect.centerx = WIDTH // 2   # // always gives an integer
+    # dvdlogo.rect.centery = HEIGHT // 2
     all_sprites = pygame.sprite.Group()
     all_sprites.add(dvdlogo)
 
@@ -57,6 +96,15 @@ def start():
                 done = True
 
 		# --- Update the world state
+        all_sprites.update()
+
+        # Take Dvdlogo and change its x-position
+        #  - x-coordinate + vel_x
+        # dvdlogo.rect.x = dvdlogo.rect.x + dvdlogo.vel_x
+
+        # Take Dvdlogo and change its y-position
+        #  - y-coord + vel_y
+        # dvdlogo.rect.y += dvdlogo.vel_y
 
         # --- Draw items
         screen.fill(BLACK)
